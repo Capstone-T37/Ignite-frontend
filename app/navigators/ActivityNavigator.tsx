2ebfb3ec066b194940ade4c6a7b689ad57225629
ayman fakri <61848289+afakri@users.noreply.github.com>
2023-09-21 22:38:09 -0400

    activity details page is now populated from the list item

--- app/navigators/ActivityNavigator.tsx
+++ app/navigators/ActivityNavigator.tsx
@@ -1,20 +1,26 @@
 import React from "react"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack"
 import {
   ActivityDetailsScreen,
   ActivityListScreen,
 } from "app/screens"
 import { colors } from "app/theme"
+import { Activity } from "app/models"
 
 export type ActivityNavigatorParamList = {
   ActivityListScreen: undefined
-  ActivityDetails: undefined
+  ActivityDetails: Activity
 }
+
+export type ActivityNavigatorScreenProps<T extends keyof ActivityNavigatorParamList> = NativeStackScreenProps<
+  ActivityNavigatorParamList,
+  T
+>
 
 const Stack = createNativeStackNavigator<ActivityNavigatorParamList>()
 export const ActivityNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false, navigationBarColor: colors.background }}>
+    <Stack.Navigator screenOptions={{ navigationBarColor: colors.background, headerShown: false }}>
       <Stack.Screen name="ActivityListScreen" component={ActivityListScreen} />
       <Stack.Screen name="ActivityDetails" component={ActivityDetailsScreen} />
     </Stack.Navigator>
--- app/screens/ActivityDetailsScreen.tsx
+++ app/screens/ActivityDetailsScreen.tsx
@@ -1,26 +1,122 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { AppStackScreenProps } from "app/navigators"
-import { Screen, Text } from "app/components"
+import { Image, TextStyle, View, ViewStyle } from "react-native"
+import { ActivityNavigatorScreenProps } from "app/navigators"
+import { Button, EmptyState, Screen, Text } from "app/components"
+import { colors, spacing, typography } from "app/theme"
+import { useSafeAreaInsetsStyle } from "app/utils/useSafeAreaInsetsStyle"
+
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "app/models"
 
-interface ActivityDetailsScreenProps extends AppStackScreenProps<"ActivityDetails"> {}
+interface ActivityDetailsScreenProps extends ActivityNavigatorScreenProps<"ActivityDetails"> { }
 
-export const ActivityDetailsScreen: FC<ActivityDetailsScreenProps> = observer(function ActivityDetailsScreen() {
+export const ActivityDetailsScreen: FC<ActivityDetailsScreenProps> = observer(function ActivityDetailsScreen(_props) {
   // Pull in one of our MST stores
   // const { someStore, anotherStore } = useStores()
+
+  const pinIcon = require("../../assets/icons/pin.png")
+  const $containerInsets = useSafeAreaInsetsStyle(["top"])
+
+
+  const { navigation, route } = _props
+  const activity = route.params
+  if (!activity) {
+    return <EmptyState
+      preset="generic"
+      style={$containerInsets}
+      headingTx="ActivityDetailsScreen.emptyStateHeading"
+      contentTx="ActivityDetailsScreen.emptyStateContent"
+      buttonTx="ActivityDetailsScreen.emptyStateButton"
+      buttonOnPress={() => navigation.goBack()}
+      ImageProps={{ resizeMode: "contain" }}
+    />
+  }
+
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
   return (
-    <Screen style={$root} preset="scroll">
-      <Text text="activityDetails" />
+    <Screen style={$screenContainer} preset="scroll"
+      safeAreaEdges={["top", "bottom"]}
+    >
+      <View style={$topContainer}>
+        <Text
+          testID="welcome-heading"
+          style={$welcomeHeading}
+          text={activity.title}
+          preset="heading"
+        />
+        <View style={$addressContainer}>
+          <Image source={pinIcon} resizeMode="contain" />
+          <Text
+            testID="welcome-subheading"
+            style={$welcomeSubHeading}
+            text={activity.address}
+            preset="subheading"
+          />
+        </View>
+      </View>
+      <View style={$descriptionContainer}>
+        <Text text={activity.description} size="md" />
+      </View>
+      <Button
+      style={$joinButton}
+        tx="ActivityDetailsScreen.joinButton"
+        onPress={() => { }}
+      //style={}
+      />
     </Screen>
   )
 })
 
-const $root: ViewStyle = {
+const $screenContainer: ViewStyle = {
   flex: 1,
+}
+
+const $topContainer: ViewStyle = {
+  flexShrink: 1,
+  flexGrow: 1,
+  flexBasis: "57%",
+  justifyContent: "center",
+}
+
+const $welcomeHeading: TextStyle = {
+  marginBottom: spacing.md,
+  alignSelf: 'center',
+}
+
+const $welcomeSubHeading: TextStyle = {
+  marginBottom: spacing.md,
+  paddingHorizontal: spacing.xs,
+  alignSelf: 'center',
+  fontFamily: typography.secondary.thin,
+  color: colors.textDim
+}
+const $addressContainer: TextStyle = {
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignContent: 'center',
+
+}
+
+const $descriptionContainer: ViewStyle = {
+  flexShrink: 1,
+  flexGrow: 0,
+  flexBasis: "43%",
+  backgroundColor: colors.palette.secondary100,
+  borderTopLeftRadius: 16,
+  borderTopRightRadius: 16,
+  paddingHorizontal: spacing.lg,
+  paddingVertical: spacing.lg,
+  justifyContent: "space-around",
+  marginVertical: spacing.xxxl
+}
+
+const $joinButton: ViewStyle = {
+  borderRadius: 17,
+  marginTop: spacing.md,
+  backgroundColor: colors.palette.secondary300,
+  paddingHorizontal: spacing.md,
+  minHeight: 32,
 }
